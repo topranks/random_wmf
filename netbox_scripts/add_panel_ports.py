3049f827542d3a1e08151c15d7cda74ffe7fb759
Cathal Mooney <cmooney@wikimedia.org>
2023-01-19 14:52:48 +0000

    Adjusted add_panel_ports to support supplying device name as arg.

--- netbox_scripts/add_panel_ports.py
+++ netbox_scripts/add_panel_ports.py
@@ -6,6 +6,7 @@ import pynetbox
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--netbox', help='Netbox server IP / Hostname', type=str, default="netbox.wikimedia.org")
 parser.add_argument('-k', '--key', help='API Token / Key', required=True, type=str)
+parser.add_argument('-p', '--panel', help='Name of panel device in Netbox', required=True, type=str)
 args = parser.parse_args()
 
 ports = ['1-2', '3-4', '5-6', '7-8', '9-10', '11-12', '13-14', '15-16', '17-18', '19-20', '21-22', '23-24']
@@ -15,7 +16,7 @@ def main():
     nb_url = "https://{}".format(args.netbox)
     nb = pynetbox.api(nb_url, token=args.key)
 
-    panel = nb.dcim.devices.get(name='EQIAD_F1_SMF_PANEL_1')
+    panel = nb.dcim.devices.get(name=args.panel)
 
     for module in range(modules):
         for port in ports:
