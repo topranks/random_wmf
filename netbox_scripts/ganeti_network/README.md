28dfcf22c9989959c7781b90ae58f6ff1aa320a8
Cathal Mooney <cmooney@wikimedia.org>
2024-10-25 15:10:30 +0100

    script for ganeit hosts

---
+++ netbox_scripts/ganeti_network/README.md
@@ -0,0 +1,68 @@
+
+Simple script to generate /etc/network/interfaces file for a given ganeti host.
+
+Also will ensure switch configuration in Netbox is correctly set up for the ganeti host.
+
+Dependencies: python3-jinja2 python3-pynetbox
+
+Example:
+```
+cmooney@wikilap:~$ ./ganeti_network.py --host ganeti1026
+Netbox API token: 
+
+Netbox: asw2-a7-eqiad interface xe-7/0/35 changed mode to tagged.
+Netbox: asw2-a7-eqiad interface xe-7/0/35 tagged vlans set to [public1-a-eqiad, analytics1-a-eqiad]
+
+
+ganeti1026 /etc/network/interfaces:
+
+*******************************************************************************
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo private public analytics
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug enp175s0f0np0
+iface private inet static
+    address 10.64.0.84/22
+    gateway 10.64.0.1
+    # dns-* options are implemented by the resolvconf package, if installed
+    dns-nameservers 10.3.0.1
+    dns-search eqiad.wmnet
+    bridge_ports    enp175s0f0np0
+    bridge_stp      off
+    bridge_maxwait  0
+    bridge_fd       0
+
+    up ip addr add 2620:0:861:101:10:64:0:84/64 dev private
+
+
+iface public inet manual
+    pre-up ip link add name 1001 link enp175s0f0np0 type vlan 1001
+    post-down ip link delete dev 1001 type vlan
+    bridge_ports   1001
+    bridge_stp     off
+    bridge_maxwait 0
+    bridge_fd      0
+    up sysctl net.ipv6.conf.public.accept_ra=0
+
+iface analytics inet manual
+    pre-up ip link add name 1030 link enp175s0f0np0 type vlan 1030
+    post-down ip link delete dev 1030 type vlan
+    bridge_ports   1030
+    bridge_stp     off
+    bridge_maxwait 0
+    bridge_fd      0
+    up sysctl net.ipv6.conf.analytics.accept_ra=0
+
+*******************************************************************************
+
+
+NOTE: Netbox vlan settings were updated, please run sre.network.configure-switch-interfaces cookbook.
+```
---
+++ netbox_scripts/ganeti_network/ganeti_network.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+
+import argparse
+from getpass import getpass
+import pynetbox
+import sys
+import ipaddress
+from jinja2 import Environment, FileSystemLoader
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--netbox', help='Netbox server IP / Hostname', type=str, default="netbox.wikimedia.org")
+parser.add_argument('-k', '--key', help='API Token / Key', type=str, default='')
+parser.add_argument('--host', help='Host to check', required=True, type=str)
+args = parser.parse_args()
+
+def main():
+    nb_url = "https://{}".format(args.netbox)
+    if args.key:
+        nb_key = args.key
+    else:
+        nb_key = getpass(prompt="Netbox API token: ")
+    global nb
+    nb = pynetbox.api(nb_url, token=nb_key)
+
+    nb_changes = False
+    host = nb.dcim.devices.get(name=args.host)
+    physical_int, switch, switch_int = get_switch_int(host)
+
+    if not str(physical_int).startswith('e'):
+        # Proper netdev name hasn't yet been imported from puppet to Netbox
+        physical_int = "<primary_int>"
+
+    # Get the private vlan host has been provisioned for
+    private_vlan = nb.ipam.vlans.get(name=switch_int.untagged_vlan.name)
+    private_vlan_location = private_vlan.name.split('-')[1]
+    if host.site.slug in ('codfw', 'eqiad') and len(private_vlan_location) > 1:
+        print(f"{host} is configured on rack-specific vlan {private_vlan} instead of row-wide... needs to be fixed manually.")
+        sys.exit(1)
+
+    # Find additional vlans the host needs to connect to
+    tagged_vlans = []
+    for vlan_type in ('public1', 'analytics1'):
+        additional_vlan_name = f"{vlan_type}-{private_vlan_location}-{host.site.slug}"
+        additional_vlan = nb.ipam.vlans.get(name=additional_vlan_name)
+        if additional_vlan is not None:
+            tagged_vlans.append(additional_vlan)
+
+    # Ensure switch port is configured correctly for these vlans
+    if switch_int.mode.value != "tagged":
+        switch_int.mode = 'tagged'
+        switch_int.save()
+        nb_changes = True
+        print(f"Netbox: {switch} interface {switch_int} changed mode to tagged.")
+    tagged_vlan_ids = set([vlan.id for vlan in tagged_vlans])
+    if tagged_vlan_ids != set([vlan.id for vlan in switch_int.tagged_vlans]):
+        switch_int.tagged_vlans = list(tagged_vlan_ids)
+        switch_int.save()
+        nb_changes = True
+        print(f"Netbox: {switch} interface {switch_int} tagged vlans set to {tagged_vlans}")
+
+    # Generate /etc/network/interfaces config from Jinja template
+    file_loader = FileSystemLoader(searchpath="./")
+    env = Environment(loader=file_loader)
+    template = env.get_template('interfaces.j2')
+    output = template.render(host=host.name,
+                             bridge_names = ['private'] + [vlan.name.split('-')[0].rstrip('1') for vlan in tagged_vlans],
+                             primary_ip4 = host.primary_ip4,
+                             primary_ip6 = host.primary_ip6,
+                             v4_gateway = ipaddress.ip_interface(host.primary_ip4).network[1],
+                             dns_search = f"{host.site.slug}.wmnet",
+                             physical_int = physical_int,
+                             tagged_vlans = tagged_vlans)
+    print(output)
+    if nb_changes:
+        print("\n\nNOTE: Netbox vlan settings were updated, please run sre.network.configure-switch-interfaces cookbook.")
+
+
+def get_switch_int(nb_host):
+    """ Returns switch and interface object for a given server primary link """
+    ip_int = nb.dcim.interfaces.get(name=nb_host.primary_ip4.assigned_object, device_id=nb_host.id)
+    if ip_int.type.value == "bridge":
+        physical_int = nb.dcim.interfaces.get(device_id=nb_host.id, bridge_id=ip_int.id,
+                                              type__n=('virtual', 'lag', 'bridge'), mgmt_only=False)
+    else:
+        physical_int = ip_int
+
+    switch = nb.dcim.devices.get(name=physical_int.connected_endpoints[0].device.name)
+    switch_int = nb.dcim.interfaces.get(device_id=switch.id, name=physical_int.connected_endpoints[0].name)
+    return physical_int, switch, switch_int
+
+
+if __name__=="__main__":
+    main()
+
