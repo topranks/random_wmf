860c232a552e0661c32fe91ada8a1e8d1dfbb044
Cathal Mooney <cmooney@wikimedia.org>
2025-07-16 09:39:38 +0100

    get wmf optics paralell

--- get_wmf_optics.py
+++ get_wmf_optics.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from jnpr.junos import Device
 from jnpr.junos.exception import ConnectError
 
@@ -13,7 +15,6 @@ import argparse
 #logging.basicConfig(level=logging.DEBUG)
 
 import requests
-import json
 import sys
 
 import yaml
@@ -29,87 +30,111 @@ def main():
     """Gets the active number of each type of SFP module in use at all of our sites"""
     routers = get_router_fqdns()
     optics = {}
-    for router, site in routers.items():
-        print(f"{router}... ", end='', flush=True)
-        # Add site to dict
-        if site not in optics:
-            optics[site] = {}
-        global junos_dev
-        junos_dev = get_junos_dev(router)
-        # Get list of interfaces so we can determine which are enabled, we don't count not-enabled as they 
-        # might be spares in the slot but unconfigured (so should count as spares not active in use
-        enabled_ints = get_enabled_ints()
 
-        fpc_info = parse_rpc_result(junos_dev.rpc.get_pic_information())
-        # SRX1600 hive brain scenario
-        if "multi-routing-engine-results" in fpc_info:
-            fpc_list = []
-            for node_fpc in fpc_info['multi-routing-engine-results']['multi-routing-engine-item']:
-                fpc_list.append(node_fpc['fpc-information']['fpc'])
-            # We hard-code the FPC from the second (last) node of the cluster as 7 to match the int naming
-            fpc_list[-1]['slot'] = "7"
-        else:
-            fpc_list = fpc_info['fpc-information']['fpc'] if type(fpc_info['fpc-information']['fpc']) == list else [fpc_info['fpc-information']['fpc']]
+    tasks = []
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        for router, site in routers.items():
+            tasks.append(executor.submit(get_router_sfp_count, router, site))
 
-        for fpc in fpc_list:
-            if fpc['state'] != "Online":
-                continue
-            fpc_slot = fpc['slot']
-            pic_list = fpc['pic'] if type(fpc['pic']) == list else [fpc['pic']]
-            for pic in pic_list:
-                if pic['pic-state'] != "Online":
-                    continue
-                pic_slot = pic['pic-slot']
-                pic_info = parse_rpc_result(junos_dev.rpc.get_pic_detail(fpc_slot=fpc_slot, pic_slot=pic_slot))
-                if "multi-routing-engine-results" in pic_info:  # SRX Hive mind, but it only has one set of results here
-                    pic_detail = pic_info['multi-routing-engine-results']['multi-routing-engine-item']['fpc-information']['fpc']['pic-detail']
-                else:
-                    pic_detail = pic_info['fpc-information']['fpc']['pic-detail']
-                if "port-information" not in pic_detail or pic_detail['port-information'] is None:
-                    # Non-modular pics (i.e. 48xGE built-in), or ones with no modules installed (empty port-information)
-                    continue
-                ports = pic_detail['port-information']['port'] if type(pic_detail['port-information']['port']) == list else [pic_detail['port-information']['port']]
-                for port_info in ports:
-                    int_ref = f"{fpc_slot}/{pic_slot}/{port_info['port-number']}"
-                    if int_ref not in enabled_ints:
-                        continue
-                    sfp_type = get_sfp_type(port_info)
-                    if sfp_type not in optics[site]:
-                        optics[site][sfp_type] = 0
-                    optics[site][sfp_type] += 1
-        junos_dev.close()
-        print("done.")
+        for task in as_completed(tasks):
+            sfps, site = task.result()
+            if site not in optics:
+                optics[site] = {}
+            for sfp_type, count in sfps.items():
+                if sfp_type not in optics[site]:
+                    optics[site][sfp_type] = 0
+                optics[site][sfp_type] += count
 
     print_results(optics)
+
+def get_router_sfp_count(router, site):
+    """Gets the number of optics of each type for a given router"""
+    optics = {}
+    junos_dev = get_junos_dev(router)
+    print(f"Connected to {router}.")
+
+    # Get list of enabled interfaces, we won't count SFPs in disabled ports as used.
+    enabled_ints = get_enabled_ints(junos_dev)
+    # Get FPC & PIC info from device
+    fpc_info = parse_rpc_result(junos_dev.rpc.get_pic_information())
+    if "multi-routing-engine-results" in fpc_info:
+        # SRX multi-node cluster will return the list of FPCs for each physical device
+        fpc_list = []
+        for node_fpc in fpc_info['multi-routing-engine-results']['multi-routing-engine-item']:
+            fpc_list.append(node_fpc['fpc-information']['fpc'])
+        # We hard-code the FPC from the second (last) node of the cluster as 7 to match the int naming
+        fpc_list[-1]['slot'] = "7"
+    else:
+        fpc_list = fpc_info['fpc-information']['fpc']
+
+    # Loop over each FPC and ask for the SFPs for each PIC
+    for fpc in fpc_list if isinstance(fpc_list, list) else [fpc_list]:
+        if fpc['state'] != "Online":
+            continue
+        for pic in fpc['pic'] if isinstance(fpc['pic'], list) else [fpc['pic']]:
+            if pic['pic-state'] != "Online":
+                continue
+            pic_info = parse_rpc_result(junos_dev.rpc.get_pic_detail(fpc_slot=fpc['slot'], pic_slot=pic['pic-slot']))
+            if "multi-routing-engine-results" in pic_info:  
+                # SRX hive mind again, but it only has one set of results here
+                pic_detail = pic_info['multi-routing-engine-results']['multi-routing-engine-item']['fpc-information']['fpc']['pic-detail']
+            else:
+                pic_detail = pic_info['fpc-information']['fpc']['pic-detail']
+            if "port-information" not in pic_detail or pic_detail['port-information'] is None:
+                # Non-modular pics (i.e. 48xGE built-in), or ones with no modules installed (empty port-information)
+                continue
+            ports = pic_detail['port-information']['port'] 
+            for port_info in ports if isinstance(ports, list) else [ports]:
+                interface_number = f"{fpc['slot']}/{pic['pic-slot']}/{port_info['port-number']}"
+                if interface_number not in enabled_ints:
+                    continue
+                sfp_type = get_sfp_type(port_info)
+                if sfp_type not in optics:
+                    optics[sfp_type] = 0
+                optics[sfp_type] += 1
+
+    junos_dev.close()
+    return optics, site
 
 
 def print_results(optics):
     """Prints the results as YAML and sorts by number of each module."""
     print()
-    sorted_data = {}
-    for site, transceivers in optics.items():
-        sorted_modules = dict(sorted(transceivers.items(), key=lambda item: item[1], reverse=True))
-        print(yaml.dump({site: sorted_modules}))
-        print()
+    sorted_modules = {}
+    # Sort the list of sites by which has the most overall SFPs
+    sorted_sites = dict(sorted(optics.items(), key=lambda item: sum(item[1].values()), reverse=True))
+    # Now sort the modules at each site and print them out
+    for site, modules in sorted_sites.items():
+        sorted_modules = dict(sorted(modules.items(), key=lambda item: item[1], reverse=True))
+        print(yaml.dump({site: sorted_modules}, sort_keys=False))
 
 
 def get_sfp_type(port_info):
-    """Returns a single string representing the SFP type based on port-info, can be as simple as the 
-       cable-type (i.e. 10GBase-LR) but we may need to set up aliases or use more of the info depending 
-       on if we have odd names coming back for some SFPs which are really the same type"""
+    """Managles the SFP names as we need to to make the output cleaner"""
+    sfp_type = port_info['cable-type'].replace("BASE ", "-")
+    alias = {
+        "100G-LR4 Lite": "100G-CWDM4",
+        "GIGE 1000LX10": "1G-LX",
+        "4X10G-LR": "4x10G-LR"
+    }
+    sfp_type = alias[sfp_type] if sfp_type in alias else sfp_type
 
-    return port_info['cable-type']
+    use_code_for = ('unknown')
+    if sfp_type.lower().startswith((use_code_for)):
+        sfp_type = port_info['sfp-vendor-pno']
+
+    return sfp_type
+
 
                 
-def get_enabled_ints():
+def get_enabled_ints(junos_dev):
     """Returns the enabled interfaces we care about"""
     interface_info = parse_rpc_result(junos_dev.rpc.get_interface_information(media=True, terse=True))
-
     enabled_ints = []
     for interface in interface_info['interface-information']['physical-interface']:
         if not interface['name'].startswith(('ge', 'xe', 'et')) or interface['admin-status'] == "down":
             continue
-        enabled_ints.append(interface['name'].split("-")[1])
+        enabled_ints.append(interface['name'].split("-")[1].split(":")[0])
 
     return enabled_ints
 
